539a3911ddb7a1056b93f0890d8e715a4d6986dc
Kogeer <gergely.kovacs02@gmail.com>
2021-07-03 22:42:34 +0200

    add comment to buildClient

--- frontend/api/build-client.js
+++ frontend/api/build-client.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const buildClient = ({ req }) => {
+    // The baseURL is the SERVICENAME.NAMESPACE.svc.cluster.local
+    // for reach the ingress-nginx from frontend service
     return axios.create({
         baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
         headers: req.headers
