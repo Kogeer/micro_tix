1606d40fe612432aac318890c25e63a380ec47d2
Kogeer <gergely.kovacs02@gmail.com>
2021-07-03 22:40:39 +0200

    refactor the index with datafetch and add create axios for ssp data fetching

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,13 @@
-const Index = () => {
-    return <h1>Landing page!</h1>;
+import buildClient from "../api/build-client";
+
+const Index = ({ data }) => {
+    return data.currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1> ;
 };
+
+export async function getServerSideProps(context) {
+    const {data} = await buildClient(context).get('/api/users/currentuser');
+
+    return {props: {data}};
+}
 
 export default Index;
