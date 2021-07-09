8e8628103a155c6a917fe2d5e077aad0bcf28141
Kogeer <gergely.kovacs02@gmail.com>
2021-07-09 18:18:03 +0200

    add create ticket complete route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from "cookie-session";
-import {errorHandler, NotFoundError} from "@kogeertix/common";
+import {errorHandler, NotFoundError, currentUser} from "@kogeertix/common";
+import {createTicketRouter} from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,10 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 );
+// Must use after cookieSession
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
