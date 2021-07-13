08e26b2a5887cce97d5cc24216557c355f639dc4
Kogeer <gergely.kovacs02@gmail.com>
2021-07-13 20:11:02 +0200

    sentence...

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,7 @@ const stan = connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    // use qGroup if service scale up, and only send one listener instance the event!!!
+    // use qGroup if service scale up, and one listener instance got the event!!!
     const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
 
     subscription.on('message', (msg: Message) => {
