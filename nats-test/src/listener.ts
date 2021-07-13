5f149c6e3252d582582fb930264d8794b08b023d
Kogeer <gergely.kovacs02@gmail.com>
2021-07-13 20:22:36 +0200

    add manual ack mode option to listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,12 @@ const stan = connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
+    // this option for save event data secure if example db connection lost
+    // and that is why we use acknowledgement mode manually
+    // after save the event data to DB we must send back to NATS streaming server
+    // we processed the event data everything is fine!
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true);
     // use qGroup if service scale up, and one listener instance got the event!!!
     const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
 
@@ -19,5 +25,7 @@ stan.on('connect', () => {
         if (typeof data === 'string') {
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
         }
+
+        msg.ack(); // processed the event data everything is fine!
     });
 });
