9090d1e9f74e0e17e9317147558ffaa5d507302a
Kogeer <gergely.kovacs02@gmail.com>
2021-07-22 21:18:55 +0200

    fix env key in tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -21,7 +21,7 @@ const start = async () => {
     }
 
     try {
-        await natsWrapper.connect(process.env.CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
             process.exit();
