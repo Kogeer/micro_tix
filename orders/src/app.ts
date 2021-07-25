dfda73b86d90d8193a7357037e59f4c190155c06
Kogeer <gergely.kovacs02@gmail.com>
2021-07-25 20:40:46 +0200

    add initial routes for orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import {json} from 'body-parser';
 import cookieSession from "cookie-session";
 import {errorHandler, NotFoundError, currentUser} from "@kogeertix/common";
 
+import {indexOrderRouter} from "./routes";
+import {newOrderRouter} from "./routes/new";
+import {showOrderRouter} from "./routes/show";
+import {deleteOrderRouter} from "./routes/delete";
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -15,6 +20,11 @@ app.use(
 );
 // Must use after cookieSession
 app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
