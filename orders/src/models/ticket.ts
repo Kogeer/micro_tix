777c7bc664f24d1be1d933bbe8596900a01217c6
Kogeer <gergely.kovacs02@gmail.com>
2021-07-29 22:17:02 +0200

    add custom query to ticketschema in orders service

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -18,6 +18,8 @@ export interface TicketDocument extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDocument> {
     build(attributes: TicketAttributes): TicketDocument;
+
+    findByEvent(event: { id: string, version: number; }): Promise<TicketDocument | null>;
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -42,6 +44,12 @@ const ticketSchema = new mongoose.Schema({
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
 
+ticketSchema.statics.findByEvent = (event: { id: string, version: number}) => {
+    return Ticket.findOne({
+        _id: event.id,
+        version: event.version - 1
+    });
+}
 ticketSchema.statics.build = (attributes: TicketAttributes) => {
     return new Ticket({
         _id: attributes.id,
