cef79169549f547208ccae4c9c6ba6c944f88113
Kogeer <gergely.kovacs02@gmail.com>
2021-10-01 22:43:58 +0200

    feat: Add create ticket page and refactor index to list tickets

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,36 @@
-import buildClient from "../api/build-client";
+const Index = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map(ticket => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        );
+    });
 
-const Index = ({ currentUser }) => {
     return (
-        <>
-            {currentUser ? (
-                <h1>You are signed in</h1>
-            ) : (
-                <h1>You are not signed in</h1>
-            )}
-        </>
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ticketList}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
 // Here we can access the client and current user from _app.js
 Index.getInitialProps = async (context, client, currentUser) => {
-    return {};
+    const {data} = await client.get('/api/tickets');
+
+    return {tickets: data};
 }
 
 export default Index;
